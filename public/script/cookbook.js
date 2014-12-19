09416eba262219f6200dab8e04cacd94fba76a55
Juha Paananen <juha.paananen@gmail.com>
2014-12-19 20:28:47 +0200

    add missing paren

--- public/script/cookbook.js
+++ public/script/cookbook.js
@@ -4,7 +4,7 @@ define([], function() {
     var square = "function square() {\n  for (var i=1; i <= 4; i++) {\n    fd(50)\n    lt(90)\n  }\n}"
     addExample("Turtle moves", "fd(50)\nlt(45)\nfd(50)\nrt(45)\nfd(50)")
     addExample("Square", square + "\nsquare()")
-    addExample("Spiral", "clear()\ni = 1\nwhile (i < 300) {\n  fd(i*1)\n  lt(i*1)\n  i++\n}"
+    addExample("Spiral", "clear()\ni = 1\nwhile (i < 300) {\n  fd(i*1)\n  lt(i*1)\n  i++\n}")
     addExample("Clear", "clear()")
 
     $("#cookbook label").click(function() {
